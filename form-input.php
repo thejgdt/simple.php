8d4c157e3f21f1ad89a827330ce1c714e557af79
ctva <theaberchio@gmail.com>
2023-11-15 19:15:16 +0700

    add input data form
    - create a HTML file that displays a form to input data mahasiswa
    - use the meta tags to define the charset, compatibility, and viewport
    - use the title tag to set the page title
    - use the center tag to align the elements to the center
    - use the h2 tag to create a heading for the form
    - use the form tag to create a form with the method and action attributes
    - use the table tag to create a table with tr and td elements
    - use the input tag to create input fields for nim, nama, jenis_kelamin, and alamat
    - use the type attribute to specify the input type
    - use the name attribute to send the data to the simpan.php script
    - use the value attribute to assign the value for the radio input
    - use the onkeyup attribute to call the isi_otomatis function
    - use the select tag to create a dropdown list of jurusan options
    - use the option tag to create the options for the select element
    - use the button tag to create a button to submit the form
    - use the value attribute to assign the value for the button

---
+++ form-input.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Membuat Form Inputan Data</title>
+</head>
+
+<center>
+
+    <body>
+        <h2>Input Data Mahasiswa</h2>
+        <form method="post" action="simpan.php">
+            <table>
+                <tr>
+                    <td>NIM</td>
+                    <td><input type="text" onkeyup="isi_otomatis()" name="nim"></td>
+                </tr>
+                <tr>
+                    <td>NAMA</td>
+                    <td><input type="text" name="nama"></td>
+                </tr>
+                <tr>
+                    <td>JENIS KELAMIN</td>
+                    <td>
+                        <input type="radio" name="jenis_kelamin" value="L">Laki-laki
+                        <input type="radio" name="jenis_kelamin" value="P">Perempuan
+                    </td>
+                </tr>
+                <tr>
+                    <td>JURUSAN</td>
+                    <td>
+                        <select name="jurusan" id="">
+                            <option value="TEKNIK INFORMATIKA">TEKNIK INFORMATIKA</option>
+                            <option value="TEKNIK MESIN">TEKNIK MESIN</option>
+                            <option value="TEKNIK KIMIA">TEKNIK KIMIA</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>ALAMAT</td>
+                    <td><input type="text" name="alamat" id=""></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><button type="submit" value="simpan">SIMPAN</button></td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</center>
+
+</html>
