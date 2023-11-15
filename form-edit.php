f1e79df74b36ae966f92816f38acb0e8e894bb70
ctva <theaberchio@gmail.com>
2023-11-15 22:00:50 +0700

    fix: viewport: improved compatibility and responsiveness

--- form-edit.php
+++ form-edit.php
@@ -19,6 +19,9 @@ function active_radio_button($value, $input)
 <html lang="en">
 
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Data Mahasiswa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
