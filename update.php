08cd170d9693af677a33b4e03bd2e23bf0af69cd
ctva <theaberchio@gmail.com>
2023-11-15 18:55:15 +0700

    fix(update.php): SQL injection vulnerability in update data script
    - use mysqli_prepare and mysqli_stmt_bind_param to prevent SQL injection
    - execute the query using mysqli_stmt_execute

--- update.php
+++ update.php
@@ -8,8 +8,10 @@ $jurusan = $_POST['jurusan'];
 $jenis_kelamin = $_POST['jenis_kelamin'];
 $alamat = $_POST['alamat'];
 
-$query = "UPDATE mahasiswa SET nim = '$nim', nama = '$nama', jurusan = '$jurusan', jenis_kelamin = '$jenis_kelamin', alamat = '$alamat' WHERE id_mahasiswa = '$id_mahasiswa'";
-mysqli_query($koneksi, $query);
+$query = "UPDATE mahasiswa SET nim = ?, nama = ?, jurusan = ?, jenis_kelamin = ?, alamat = ? WHERE id_mahasiswa = ?";
+$stmt = mysqli_prepare($koneksi, $query);
+mysqli_stmt_bind_param($stmt, "sssssi", $nim, $nama, $jurusan, $jenis_kelamin, $alamat, $id_mahasiswa);
+mysqli_stmt_execute($stmt);
 
 header("location:index.php");
 ?>
