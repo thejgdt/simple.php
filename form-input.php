52ec13928d1a2ed5ea37ff0d9365e9829136505d
ctva <theaberchio@gmail.com>
2023-11-15 21:53:45 +0700

    style(form-input.php): implement bootstrap for better UI

--- form-input.php
+++ form-input.php
@@ -5,34 +5,74 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Membuat Form Inputan Data</title>
+    <title>Input Data Mahasiswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 8px;
+        }
+
+        table {
+            width: 50%;
+            margin: auto;
+            margin-top: 20px;
+        }
+
+        td {
+            padding: 10px;
+        }
+
+        button {
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 16px;
+            transition: background-color 0.3s;
+        }
+
+        button:hover {
+            background-color: #333;
+        }
+    </style>
 </head>
 
-<center>
+<body>
 
-    <body>
-        <h2>Input Data Mahasiswa</h2>
+    <center>
+        <h2 class="mt-4 mb-4">Input Data Mahasiswa</h2>
         <form method="post" action="simpan.php">
-            <table>
+            <table class="table table-bordered table-striped">
                 <tr>
                     <td>NIM</td>
-                    <td><input type="text" onkeyup="isi_otomatis()" name="nim"></td>
+                    <td><input type="text" class="form-control" onkeyup="isi_otomatis()" name="nim"></td>
                 </tr>
                 <tr>
                     <td>NAMA</td>
-                    <td><input type="text" name="nama"></td>
+                    <td><input type="text" class="form-control" name="nama"></td>
                 </tr>
                 <tr>
                     <td>JENIS KELAMIN</td>
                     <td>
-                        <input type="radio" name="jenis_kelamin" value="L">Laki-laki
-                        <input type="radio" name="jenis_kelamin" value="P">Perempuan
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="jenis_kelamin" value="L">
+                            <label class="form-check-label">Laki-laki</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="jenis_kelamin" value="P">
+                            <label class="form-check-label">Perempuan</label>
+                        </div>
                     </td>
                 </tr>
                 <tr>
                     <td>JURUSAN</td>
                     <td>
-                        <select name="jurusan" id="">
+                        <select class="form-select" name="jurusan" id="">
                             <option value="TEKNIK INFORMATIKA">TEKNIK INFORMATIKA</option>
                             <option value="TEKNIK MESIN">TEKNIK MESIN</option>
                             <option value="TEKNIK KIMIA">TEKNIK KIMIA</option>
@@ -41,14 +81,16 @@
                 </tr>
                 <tr>
                     <td>ALAMAT</td>
-                    <td><input type="text" name="alamat" id=""></td>
+                    <td><input type="text" class="form-control" name="alamat" id=""></td>
                 </tr>
                 <tr>
-                    <td colspan="2"><button type="submit" value="simpan">SIMPAN</button></td>
+                    <td colspan="2"><button type="submit" class="btn btn-success" value="simpan">SIMPAN</button></td>
                 </tr>
             </table>
         </form>
-    </body>
-</center>
+    </center>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 
 </html>
