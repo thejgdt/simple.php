a0e6790b74050278dc4efffdc8a4da33ac8d4b2e
ctva <theaberchio@gmail.com>
2023-11-15 18:53:02 +0700

    add update data script
    - include the database connection script
    - get the data from the POST request
    - create a query to update the data in the mahasiswa table where id_mahasiswa matches

---
+++ update.php
@@ -0,0 +1,15 @@
+<?php
+include 'koneksi.php';
+
+$id_mahasiswa = $_POST['id_mahasiswa'];
+$nim = $_POST['nim'];
+$nama = $_POST['nama'];
+$jurusan = $_POST['jurusan'];
+$jenis_kelamin = $_POST['jenis_kelamin'];
+$alamat = $_POST['alamat'];
+
+$query = "UPDATE mahasiswa SET nim = '$nim', nama = '$nama', jurusan = '$jurusan', jenis_kelamin = '$jenis_kelamin', alamat = '$alamat' WHERE id_mahasiswa = '$id_mahasiswa'";
+mysqli_query($koneksi, $query);
+
+header("location:index.php");
+?>
