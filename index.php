1cc8e2c9962a2600324c5b4c02fec04fed8be94d
ctva <theaberchio@gmail.com>
2023-11-15 20:49:07 +0700

    implement bootstrap styling to the index page
    - added Bootstrap CSS and JS files from a CDN
    - replaced the table classes with Bootstrap classes for styling
    - used Bootstrap's button styles (btn classes) for the action buttons
    - the "Masukkan Data Lainnya" button now has a Bootstrap success button style (btn-success)

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List Mahasiswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
             font-family: 'Arial', sans-serif;
@@ -91,47 +92,53 @@
 </head>
 
 <body>
-    <header>
+    <header class="bg-dark text-white">
         <h1>List Mahasiswa</h1>
     </header>
 
-    <section>
-        <table>
-            <tr>
-                <th>NO</th>
-                <th>NIM</th>
-                <th>NAMA</th>
-                <th>GENDER</th>
-                <th>JURUSAN</th>
-                <th>ACTION</th>
-            </tr>
+    <section class="container">
+        <table class="table table-bordered table-striped">
+            <thead class="thead-dark">
+                <tr>
+                    <th>NO</th>
+                    <th>NIM</th>
+                    <th>NAMA</th>
+                    <th>GENDER</th>
+                    <th>JURUSAN</th>
+                    <th>ACTION</th>
+                </tr>
+            </thead>
+            <tbody>
 
-            <?php
-            include 'koneksi.php';
-            $mahasiswa = mysqli_query($koneksi, "SELECT * FROM mahasiswa");
-            $no = 1;
-            foreach ($mahasiswa as $row) {
-                $jenis_kelamin = $row['jenis_kelamin'] == 'p' ? 'perempuan' : 'laki-laki';
-                echo "<tr>
-                    <td>$no</td>
-                    <td>" . $row['nim'] . "</td>
-                    <td>" . $row['nama'] . "</td>
-                    <td>" . $jenis_kelamin . "</td>
-                    <td>" . $row['jurusan'] . "</td>
-                    <td>
-                        <a class='action-btn edit-btn' href='form-edit.php?id_mahasiswa=$row[id_mahasiswa]'>Edit</a>
-                        <a class='action-btn delete-btn' href='delete.php?id_mahasiswa=$row[id_mahasiswa]'>Delete</a>
-                    </td>
-                    </tr>";
-                $no++;
-            }
-            ?>
+                <?php
+                include 'koneksi.php';
+                $mahasiswa = mysqli_query($koneksi, "SELECT * FROM mahasiswa");
+                $no = 1;
+                foreach ($mahasiswa as $row) {
+                    $jenis_kelamin = $row['jenis_kelamin'] == 'p' ? 'perempuan' : 'laki-laki';
+                    echo "<tr>
+                        <td>$no</td>
+                        <td>" . $row['nim'] . "</td>
+                        <td>" . $row['nama'] . "</td>
+                        <td>" . $jenis_kelamin . "</td>
+                        <td>" . $row['jurusan'] . "</td>
+                        <td>
+                            <a class='btn btn-primary' href='form-edit.php?id_mahasiswa=$row[id_mahasiswa]'>Edit</a>
+                            <a class='btn btn-danger' href='delete.php?id_mahasiswa=$row[id_mahasiswa]'>Delete</a>
+                        </td>
+                        </tr>";
+                    $no++;
+                }
+                ?>
+            </tbody>
         </table>
 
-        <button>
-            <a href="form-input.php">Masukkan Data Lainnya</a>
+        <button class="btn btn-success">
+            <a href="form-input.php" class="text-white">Masukkan Data Lainnya</a>
         </button>
     </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
