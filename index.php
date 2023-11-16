b2ce73e9e756a41912f98d1d339b4ee244553187
ctva <theaberchio@gmail.com>
2023-11-16 11:22:05 +0700

    feat: merge form-input into index page

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
         td {
             border: 1px solid #ddd;
             padding: 8px;
-            text-align: center;
+            text-align: auto;
         }
 
         th {
@@ -109,6 +109,18 @@
         .delete-btn:hover {
             background-color: #333;
         }
+
+        .table {
+            vertical-align: middle;
+        }
+
+        .mb-1 {
+            margin: .25rem !important;
+        }
+
+        #table-bottom {
+            text-align: center;
+        }
     </style>
 </head>
 
@@ -136,7 +148,50 @@
     </nav>
 
     <section class="container">
-        <div class="table-responsive">
+        <h2 class="mt-4 mb-4">Input Data Mahasiswa</h2>
+        <form method="post" action="simpan.php">
+            <table class="table table-bordered table-striped">
+                <tr>
+                    <td>NIM</td>
+                    <td><input type="text" class="form-control" onkeyup="isi_otomatis()" name="nim"></td>
+                </tr>
+                <tr>
+                    <td>NAMA</td>
+                    <td><input type="text" class="form-control" name="nama"></td>
+                </tr>
+                <tr>
+                    <td>JENIS KELAMIN</td>
+                    <td>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="jenis_kelamin" value="L">
+                            <label class="form-check-label">Laki-laki</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="jenis_kelamin" value="P">
+                            <label class="form-check-label">Perempuan</label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>JURUSAN</td>
+                    <td>
+                        <select class="form-select" name="jurusan" id="">
+                            <option value="TEKNIK INFORMATIKA">TEKNIK INFORMATIKA</option>
+                            <option value="TEKNIK MESIN">TEKNIK MESIN</option>
+                            <option value="TEKNIK KIMIA">TEKNIK KIMIA</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>ALAMAT</td>
+                    <td><input type="text" class="form-control" name="alamat" id=""></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><button type="submit" class="btn btn-success" value="simpan">SIMPAN</button></td>
+                </tr>
+            </table>
+        </form>
+        <div id="table-bottom" class="table-responsive">
             <table class="table table-bordered table-striped">
                 <thead class="thead-dark">
                     <tr>
@@ -175,10 +230,6 @@
                 </tbody>
             </table>
         </div>
-
-        <button class="btn btn-success">
-            <a href="form-input.php" class="text-white">Masukkan Data Lainnya</a>
-        </button>
     </section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
