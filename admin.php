2862c7df84f67192671b4e0cd03075794f37aded
ctva <theaberchio@gmail.com>
2023-11-17 09:51:48 +0700

    feat: add admin page with bootstrap

---
+++ admin.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Universitas Swasta Barat</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <header class="bg-dark text-white">
+        <h1>THEJGDT</h1>
+    </header>
+
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link home" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mahasiswa" href="#">Mahasiswa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link admin" href="#">Admin</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
