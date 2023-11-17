73018bc87b17ace1de6a0b8167137d6db864432f
ctva <theaberchio@gmail.com>
2023-11-17 10:00:21 +0700

    refactor(index.php): add internal styles

--- index.php
+++ index.php
@@ -7,6 +7,109 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Universitas Swasta Barat</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1em 0;
+        }
+
+        .navbar {
+            background-color: #333;
+        }
+
+        .navbar a {
+            color: white;
+            padding: 14px 16px;
+            text-decoration: none;
+            text-align: center;
+        }
+
+        .navbar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .navbar a.active {
+            background-color: #ffffff;
+            color: #000000;
+        }
+
+        section {
+            margin: 20px;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th,
+        td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: center;
+        }
+
+        th {
+            background-color: #4CAF50;
+            color: white;
+        }
+
+        button {
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 16px;
+            margin-top: 10px;
+            transition: background-color 0.3s;
+        }
+
+        button:hover {
+            background-color: #333;
+        }
+
+        button a {
+            text-decoration: none;
+            color: white;
+        }
+
+        .action-btn {
+            display: inline-block;
+            padding: 8px 16px;
+            margin-right: 5px;
+            margin-bottom: 5px;
+            text-decoration: none;
+            color: #fff;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+        }
+
+        .edit-btn {
+            background-color: #4285F4;
+        }
+
+        .delete-btn {
+            background-color: #d32f2f;
+        }
+
+        .edit-btn:hover,
+        .delete-btn:hover {
+            background-color: #333;
+        }
+    </style>
 </head>
 
 <body>
